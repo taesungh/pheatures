6715a019b6b9f9bdb3f371b274bac799e2fd4793
Taesung Hwang <taesungh@uci.edu>
2022-05-07 17:02:38 -0700

    Make message alerts expandable like accordion

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -1,6 +1,13 @@
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
+
 import Message from "pheatures/Message";
+
+import "./Messages.css";
 
 interface MessagesProps {
   messages: Message[];
@@ -8,10 +15,26 @@ interface MessagesProps {
 
 function Messages({ messages }: MessagesProps) {
   return (
-    <Stack className="messages">
+    <Stack>
       {messages.map((message, i) => (
-        <Alert key={i} severity={message.type}>
-          {message.title}
+        <Alert key={i} className="accordion-alert" severity={message.type}>
+          <Accordion
+            disableGutters
+            elevation={0}
+            sx={{ background: "none", color: "inherit" }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              sx={{ minHeight: "0", padding: "0" }}
+              aria-controls={`panel-content-${i}`}
+              id={`panel-header-${i}`}
+            >
+              {message.title}
+            </AccordionSummary>
+            <AccordionDetails sx={{ padding: "8px 16px" }}>
+              {message.detail}
+            </AccordionDetails>
+          </Accordion>
         </Alert>
       ))}
     </Stack>
