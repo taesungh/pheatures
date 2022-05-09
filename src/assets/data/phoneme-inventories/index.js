d9de4d8827a77decdae678f1de67b76f655b6917
Taesung Hwang <taesungh@uci.edu>
2022-05-08 20:06:46 -0700

    Create inventory selection system
    - Upload inventories provided in download bundle

---
+++ src/assets/data/phoneme-inventories/index.js
@@ -0,0 +1,27 @@
+import EnglishNoDiphthongs from "./EnglishNoDiphthongs.inv";
+import German from "./German.inv";
+import HypotheticalLanguage from "./HypotheticalLanguage.inv";
+import Ilokano from "./Ilokano.inv";
+import Indonesian from "./Indonesian.inv";
+import Japanese from "./Japanese.inv";
+import Korean from "./Korean.inv";
+import Lango from "./Lango.inv";
+import Lomongo from "./Lomongo.inv";
+import Spanish from "./Spanish.inv";
+import TobaBatak from "./TobaBatak.inv";
+import Turkish from "./Turkish.inv";
+
+export default {
+  EnglishNoDiphthongs,
+  German,
+  HypotheticalLanguage,
+  Ilokano,
+  Indonesian,
+  Japanese,
+  Korean,
+  Lango,
+  Lomongo,
+  Spanish,
+  TobaBatak,
+  Turkish,
+};
--- src/components/inventory-editor/InventoryEditor.tsx
+++ src/components/inventory-editor/InventoryEditor.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-import Box from "@mui/material/Box";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
+import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
 
-import ComplexSymbol from "pheatures/ComplexSymbol";
 import BaseSymbolList from "pheatures/BaseSymbolList";
+import ComplexSymbol from "pheatures/ComplexSymbol";
 import Diacritics from "pheatures/Diacritics";
 import PhonemeInventory from "pheatures/PhonemeInventory";
 import useFileData from "utils/useFileData";
+
+import inventories from "assets/data/phoneme-inventories";
 
 interface InventoryEditorProps {
   symbolList: BaseSymbolList;
@@ -18,26 +26,68 @@ interface InventoryEditorProps {
 }
 
 function InventoryEditor({ symbolList, diacritics, symbols, setSymbols }: InventoryEditorProps) {
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<File | string>();
   const fileData = useFileData<string[]>(file, { delimiter: "\t" });
 
+  // when fileData changes, process the data into a PhonemeInventory
+  // call setSymbols with the symbols of the inventory
   useEffect(() => {
     const inventory = PhonemeInventory.fromData(fileData, symbolList, diacritics);
     setSymbols(inventory.symbols);
   }, [fileData, symbolList, diacritics, setSymbols]);
 
+  const [inventoryName, setInventoryName] = useState<string>("");
+
+  const handleSelectInventory = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // update the selection value
+    setInventoryName(event.target.value);
+    // set the file path which will update fileData which will trigger the previous effect
+    setFile(event.target.value);
+  };
+
   const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files?.length) {
-      setFile(event.target.files[0]);
+      const file = event.target.files[0];
+      setFile(file);
+      // set the selection to the name of the file
+      setInventoryName(file.name);
     }
   };
 
+  const fileUploaded = typeof file === "object";
+
   return (
-    <Box>
+    <Stack direction="row" alignItems="center" justifyContent="center" spacing={3}>
+      <TextField
+        value={inventoryName}
+        onChange={handleSelectInventory}
+        select
+        id="inventory-select"
+        label="Phoneme Inventory"
+        // change color if uploaded rather than selected
+        inputProps={{ sx: { color: fileUploaded ? "info.dark" : "common.black" } }}
+        sx={{ width: "20rem" }}
+      >
+        {Object.entries(inventories).map(([name, path]) => (
+          <MenuItem key={name} value={path}>
+            {name}
+          </MenuItem>
+        ))}
+        {fileUploaded && <MenuItem value={file.name}>{file.name} (uploaded)</MenuItem>}
+      </TextField>
+
       <label htmlFor="inventory-file-upload">
-        <Input id="inventory-file-upload" type="file" onChange={handleUpload} />
+        <Input
+          id="inventory-file-upload"
+          type="file"
+          onChange={handleUpload}
+          sx={{ display: "none" }}
+        />
+        <Button startIcon={<FileUploadRoundedIcon />} component="span">
+          Upload
+        </Button>
       </label>
-    </Box>
+    </Stack>
   );
 }
 
