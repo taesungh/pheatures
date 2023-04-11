a13c8118ba70e89d2eb7377a4d91faa8a45027c1
Taesung Hwang <taesungh@uci.edu>
2023-04-11 14:49:17 -0700

    Stylize inventory editor cell phonemes
    - Use phoneme typography variant
    - Hide checkbox and make label take up entire table cell
    - Change background color of cell based on selection
      - Additional work is needed on `:focus` states

--- src/components/inventory-editor/InventoryTable.tsx
+++ src/components/inventory-editor/InventoryTable.tsx
@@ -1,11 +1,15 @@
+import { cyan } from "@mui/material/colors";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 
 import ComplexSymbol from "pheatures/ComplexSymbol";
 import IPASkeleton from "pheatures/IPASkeleton";
+
+import "./InventoryTable.css";
 
 interface InventoryTableProps {
 	skeleton: IPASkeleton;
@@ -15,14 +19,21 @@ interface InventoryTableProps {
 
 function InventoryTable({ skeleton, selected, handleSelect }: InventoryTableProps) {
 	const SymbolCell = ({ symbol }: { symbol: ComplexSymbol }) => (
-		<TableCell>
-			<label>
+		<TableCell
+			align="center"
+			// :has selector is not supported in Firefox :(
+			sx={{
+				padding: 0,
+				backgroundColor: selected.includes(symbol) ? cyan[200] : "transparent",
+			}}
+		>
+			<label className="symbolLabel">
 				<input
 					type="checkbox"
 					checked={selected.includes(symbol)}
 					onChange={() => handleSelect(symbol)}
 				/>
-				<span>{symbol.displayCharacter}</span>
+				<Typography variant="phoneme">{symbol.displayCharacter}</Typography>
 			</label>
 		</TableCell>
 	);
