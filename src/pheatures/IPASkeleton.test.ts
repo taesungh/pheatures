47d888e96926ca35062d4b9f4783108f5dacdeee
Taesung Hwang <taesungh@uci.edu>
2024-11-17 23:31:15 -0800

    Simplify input data for `IPASkeleton.test.ts`
    - Parse TSV data without headers and use directly in `IPASkeleton`

--- src/pheatures/IPASkeleton.test.ts
+++ src/pheatures/IPASkeleton.test.ts
@@ -1,20 +1,16 @@
 import { expect, test } from "vitest";
 
 import consonantChartsPath from "@/assets/data/ipa-chart/ipachart-consonants.tsv";
+import parseFile from "@/utils/dataTransformer";
 
 import ComplexSymbol from "./ComplexSymbol";
 import { diacriticList } from "./Diacritics";
 import IPASkeleton from "./IPASkeleton";
+
 import symbolList from "./SymbolList.test";
-import parseFile from "@/utils/dataTransformer";
 
-const consonantsChart = parseFile<Record<string, string>>(consonantChartsPath, true);
-
-// unwrap headers provided by dataTransformer
-const rawConsonants = [Array.from(Object.keys(consonantsChart[0]))].concat(
-	consonantsChart.map(Object.values)
-);
-const skeleton = new IPASkeleton(rawConsonants, symbolList);
+const consonantsChart = parseFile<string[]>(consonantChartsPath, false);
+const skeleton = new IPASkeleton(consonantsChart, symbolList);
 
 test("finds reasonable base candidate: aspirated backed velar plosive", async () => {
 	const symbol = ComplexSymbol.fromBaseSymbol(symbolList.symbols["k"], [
